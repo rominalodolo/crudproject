c239ffb726912c93db916197e3aa53e8706be69a
rominalodolo <lodoloromina@gmail.com>
2021-12-06 19:55:05 +0200

    app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import "./App.css";
+import * as firebase from "firebase";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import * as firebase from "firebase";
+class App extends Component {
+  constructor() {
+    super();
+    console.log(firebase);
+  }
+  render() {
+    return (
+      <div>
+        <h1></h1>
+      </div>
+    );
+  }
 }
-
 export default App;
 
 
