34d3a447328ba2fba293e169ed1c49e829366f11
rominalodolo <lodoloromina@gmail.com>
2021-12-06 21:09:41 +0200

    user

--- src/User.js
+++ src/User.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import * as firebase from "firebase";
 import { Table, Button, Modal } from "react-bootstrap";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 class User extends Component {
   constructor(props) {
@@ -11,16 +11,60 @@ class User extends Component {
       showDeleteDialog: false,
       selectedUser: {},
     };
+    this.add = this.add.bind(this);
     this.closeDeleteDialog = this.closeDeleteDialog.bind(this);
     this.delete = this.delete.bind(this);
   }
 
   componentDidMount() {
-    firebase.database().ref("/");
-    (103).on("value", (snapshot) => {
-      console.log(snapshot.val());
+    firebase
+      .database()
+      .ref("/")
+      .on("value", (snapshot) => {
+        let returnArr = [];
+        snapshot.forEach((data) => {
+          var user = data.val();
+          user["key"] = data.key;
+          returnArr.push(user);
+        });
+        this.setState({
+          users: returnArr,
+        });
+      });
+  }
+
+  add(e) {
+    this.props.history.push("/add");
+  }
+
+  openDeleteDialog(user) {
+    this.setState({
+      showDeleteDialog: true,
+      selectedUser: user,
     });
   }
+  delete(e) {
+    firebase
+      .database()
+      .ref("/" + this.state.selectedUser.key)
+      .remove()
+      .then((x) => {
+        console.log("SUCCESS");
+        this.closeDeleteDialog();
+      })
+      .catch((error) => {
+        alert("Could not delete the user.");
+        console.log("ERROR", error);
+      });
+  }
+
+  closeDeleteDialog() {
+    this.setState({
+      showDeleteDialog: false,
+      selectedUser: {},
+    });
+  }
+
   render() {
     const listUsers = this.state.users.map((user) => (
       <tr key={user.key}>
