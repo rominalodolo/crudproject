6037ff00952df732fb5f4a14e17973921194f03c
rominalodolo <lodoloromina@gmail.com>
2021-12-06 19:37:39 +0200

    adds

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import * as firebase from 'firebase';
 
+var config = {
+  apiKey: "AIzaSyBN9WlmRc3SedmC4agM1G-rYqezGR22iZE",
+  authDomain: "crudproject-45834.firebase app.com",
+  databaseURL: "https://crudproject-45834.firebaseio.com",
+  projectId: "crudproject-45834",
+  storageBucket: "crudproject-45834.appspot.com",
+  messagingSenderId: "590481645308"
+  appId: “…”
+};
+
+firebase.initializeApp(config);
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
+
+
 reportWebVitals();
