6610f1051ec1e37412835a0b5190e4e50b27a633
rominalodolo <lodoloromina@gmail.com>
2022-01-12 17:38:17 +0200

    update

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import firebase from "firebase";
 
-var config = {
+const firebaseConfig = {
   apiKey: "AIzaSyAH4CjXrXm51t8ZeaKUfAB1XYPpFxKpzOo",
   authDomain: "crudbeginningreact.firebaseapp.com",
   databaseURL: "https://crudbeginningreact-default-rtdb.firebaseio.com",
@@ -13,10 +13,22 @@ var config = {
   storageBucket: "crudbeginningreact.appspot.com",
   messagingSenderId: "536739765095",
   appId: "1:536739765095:web:1f1a2b5128492fe9b8adc2",
-  measurementId: "G-FCSG8N2B79",
+  measurementId: "G-FCSG8N2B79"
 };
 
 firebase.initializeApp(config);
 ReactDOM.render(<App />, document.getElementById("root"));
 
 reportWebVitals();
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
