1d6cb09fb1041b971258c931e2ce5206e281a349
Romina <83961643+rominalodolo@users.noreply.github.com>
2022-01-19 16:45:04 +0200

    Update firebase_config.js

--- src/firebase_config.js
+++ src/firebase_config.js
@@ -4,14 +4,8 @@
 // import app from './App';s
 
 // const firebaseConfig = {
-//     apiKey: "AIzaSyAH4CjXrXm51t8ZeaKUfAB1XYPpFxKpzOo",
-//     authDomain: "crudbeginningreact.firebaseapp.com",
-//     databaseURL: "https://crudbeginningreact-default-rtdb.firebaseio.com",
-//     projectId: "crudbeginningreact",
-//     storageBucket: "crudbeginningreact.appspot.com",
-//     messagingSenderId: "536739765095",
-//     appId: "1:536739765095:web:1f1a2b5128492fe9b8adc2",
-//     measurementId: "G-FCSG8N2B79",
+  // data has been removed for saftey reasons, fetch api details from firebase data settings 
+
 //   };
   
 //   const app = initializeApp(firebaseConfig);
